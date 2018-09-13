f4bf5416861c49c04acd71e15bf1a642cbd63d41
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2018-09-13 18:26:50 +0200

    feat: add kotlin android extensions

--- app/src/main/java/com/igomezal/cursokotlin/activities/KotlinAndroidExtensionsActivity.kt
+++ app/src/main/java/com/igomezal/cursokotlin/activities/KotlinAndroidExtensionsActivity.kt
@@ -2,12 +2,20 @@ package com.igomezal.cursokotlin.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.igomezal.cursokotlin.R
+import kotlinx.android.synthetic.main.activity_kotlin_android_extensions.*
 
 class KotlinAndroidExtensionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_android_extensions)
+
+        val btn = findViewById<Button>(R.id.buttonById)
+        btn.setOnClickListener{ Toast.makeText(this, "Click By ID!", Toast.LENGTH_LONG).show() }
+
+        buttonByKAT?.setOnClickListener{ Toast.makeText(this, "Click by KAT!", Toast.LENGTH_LONG).show() }
     }
 }
